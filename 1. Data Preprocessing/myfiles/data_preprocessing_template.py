1d60a5dc6353a7ef7e75ec3d442640ca29df7203
Nebulino <l.mcclermont@gmail.com>
2018-02-06 12:51:14 +0100

    Add files via upload

---
+++ 1. Data Preprocessing/myfiles/data_preprocessing_template.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3:].values
+
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(x[:, 1:3])
+
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+
+
